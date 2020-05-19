3e9e659d01f43fb01ead65aff7deab4c355ce855
Sviatoslav Kliuchev <svyatoslav.klyuchev@gmail.com>
2020-05-20 02:14:17 +0300

    ..

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-const express = require('express')
+// const express = require('express')
+const http = require('http');
 const path = require('path')
 const PORT = process.env.PORT || 80
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+http.createServer((req, res) => {
+    if (req.url == "/") {
+        res.end(`<html><body><a href="/test">TEST LINK</a></body></html>`)
+    } else {
+        res.end(`
+        {
+            "applinks": {
+                "apps": [],
+                "details": [
+                    {
+                        "appID": "NRLYRM2NLJ.com.kliuchev.UniversalLinks",
+                        "paths": ["*"]
+                    }
+                ]
+            }
+        } 
+        `)
+    }
+}).listen(80);
